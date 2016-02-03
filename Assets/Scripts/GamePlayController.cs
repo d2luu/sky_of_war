ccea02450996dc2538b4e9b0c11f64acc8de9496
Dao Danh Luu <luubkk57@gmail.com>
2016-02-03 14:09:13 +0700

    Fix Conflick

---
+++ Assets/Scripts/GamePlayController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GamePlayController : MonoBehaviour {
+	//public GameObject BG_map;
+	public float speedMap = 0.1f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 offset = new Vector2 (0,Time.time * speedMap);
+
+	}
+
+}
