617d5dacbf27de94fef93150bf3143c36b23a744
HaiNguyen <mr.haicaca@gmail.com>
2016-02-20 13:19:44 +0700

    upbaicua Hai

--- Assets/Scripts/Vehicle.cs
+++ Assets/Scripts/Vehicle.cs
@@ -1,37 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+
 // yeu cau phai co thanh phan Rigidbody2D
 [RequireComponent(typeof(Rigidbody2D))]
 public class Vehicle : MonoBehaviour{
 	public float speed;
-	public int healthy;
-	public float shotDelay;
-	public GameObject bullet;
-	public GameObject explosion;
-
-	//hieu ung no
-	//public void Explosion(){
-	//	Instantiate (explosion,transform.position,transform.rotation);
-	//}
-
-	// move palayer
-	public void Move (Vector2 direction){
-		GetComponent<Rigidbody2D> ().velocity = direction * speed;
-	}
-
-	// tao dan
-	public void Shot (Transform origin){
-		Instantiate (bullet,origin.position,origin.rotation);
-	}
+	public float healthy;
 	public bool checkDestroy(){
 		if (healthy <= 0) {
 			return true;
 		}
 		return false;
 	}
-	public void plugHealth(int HP){
+	public void plugHealth(float HP){
 		healthy += HP;
+		Debug.Log("Healthy:" + healthy);
 	}
-	//Phuong thuc destroy() sau se viet them khi tiem hieu ve animation
 }
  
